07d7498e31b039047705006e0d0ecdb7f6ddb2a2
jorelanbo <jorelanbo@126.com>
2019-05-31 16:14:42 +0800

    改进动态权限功能

--- app/src/main/java/com/js/sample/utils/PermissionUtil.java
+++ app/src/main/java/com/js/sample/utils/PermissionUtil.java
@@ -14,6 +14,8 @@ public class PermissionUtil {
     public static final int REQUEST_LOCATION = 2;
     public static final int REQUEST_CAMERA = 3;
     public static final int REQUEST_IMEI = 4;
+    public static final int REQUEST_INTERNET = 5;
+
 
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -34,11 +36,15 @@ public class PermissionUtil {
             Manifest.permission.READ_PHONE_STATE
     };
 
+    private static String[] PERMISSION_INTERNET = {
+            Manifest.permission.INTERNET
+    };
+
     /**
      * 检查内部存储读写权限
      * @param context c
      */
-    public static void checkExternalStoragePermission (Context context) {
+    public static boolean checkExternalStoragePermission (Context context) {
         try {
             //检测是否有写的权限
             int permission = ActivityCompat.checkSelfPermission(context,
@@ -46,17 +52,20 @@ public class PermissionUtil {
             if (permission != PackageManager.PERMISSION_GRANTED) {
                 // 没有写的权限，去申请写的权限，会弹出对话框
                 ActivityCompat.requestPermissions((Activity) context, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+            } else {
+                return true;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return false;
     }
 
     /**
      * 检查拍照权限
      * @param context c
      */
-    public static void checkCameraPermission (Context context) {
+    public static boolean checkCameraPermission (Context context) {
         try {
             //检测是否有使用相机的权限
             int permission = ActivityCompat.checkSelfPermission(context,
@@ -64,17 +73,20 @@ public class PermissionUtil {
             if (permission != PackageManager.PERMISSION_GRANTED) {
                 // 没有使用相机的权限，去申请权限，会弹出对话框
                 ActivityCompat.requestPermissions((Activity) context, PERMISSIONS_CAMERA, REQUEST_CAMERA);
+            } else {
+                return true;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return false;
     }
 
     /**
      * 检查定位权限
      * @param context c
      */
-    public static void checkLocationPermission (Context context) {
+    public static boolean checkLocationPermission (Context context) {
         try {
             //检测是否有获取位置的权限
             int permission1 = ActivityCompat.checkSelfPermission(context,
@@ -84,17 +96,20 @@ public class PermissionUtil {
             if (permission1 != PackageManager.PERMISSION_GRANTED && permission2 != PackageManager.PERMISSION_GRANTED) {
                 // 没有获取位置的权限，去申请获取位置的权限，会弹出对话框
                 ActivityCompat.requestPermissions((Activity) context, PERMISSIONS_LOCATION, REQUEST_LOCATION);
+            } else {
+                return true;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return false;
     }
 
     /**
      * 检查手机状态信息的权限
      * @param context c
      */
-    public static void checkGetIMEIPermission (Context context) {
+    public static boolean checkGetIMEIPermission (Context context) {
         try {
             //检测是否有获取手机状态的权限
             int permission = ActivityCompat.checkSelfPermission(context,
@@ -102,9 +117,29 @@ public class PermissionUtil {
             if (permission != PackageManager.PERMISSION_GRANTED) {
                 // 没有获取手机状态的权限，去申请权限，会弹出对话框
                 ActivityCompat.requestPermissions((Activity) context, PERMISSION_IMEI, REQUEST_IMEI);
+            } else {
+                return true;
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return false;
+    }
+
+    public static boolean checkInternetPermission (Context context) {
+        try {
+            //检测是否有获取手机状态的权限
+            int permission = ActivityCompat.checkSelfPermission(context,
+                    Manifest.permission.INTERNET);
+            if (permission != PackageManager.PERMISSION_GRANTED) {
+                // 没有获取手机状态的权限，去申请权限，会弹出对话框
+                ActivityCompat.requestPermissions((Activity) context, PERMISSION_INTERNET, REQUEST_INTERNET);
+            } else {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
